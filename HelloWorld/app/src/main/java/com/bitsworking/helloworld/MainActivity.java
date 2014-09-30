f2820c578c735b2e090c8ca58a5e6f651c3d553d
aw13k001 <aw13k001@technikum-wien.at>
2014-09-30 10:13:53 +0200

    LongClickListener und Kommentare eingefügt

--- HelloWorld/app/src/main/java/com/bitsworking/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/bitsworking/helloworld/MainActivity.java
@@ -12,17 +12,31 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     EditText mEditText;
-    Button mButton;
+    Button myFancyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);   // hier wird die Oberfläche gesetzt
 
-        mEditText = (EditText) findViewById(R.id.myFancyInput);
-        mButton = (Button) findViewById(R.id.myFancyButton);
+        mEditText = (EditText) findViewById(R.id.myFancyInput);  // findViewById - bekomme von der Oberfläche über die Id der Oberfläche, liefert eine Signatur zurück und der wird dann auf den datentyp EditBox umgewandelt = Casten
+        myFancyButton = (Button) findViewById(R.id.myFancyButton);  // findViewById liefert eine View zurück und die muss ich dann auf das richtige Objetk casten mit ()
+        myFancyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+        /* Wenn man den Button lange drückt */
+        myFancyButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getApplicationContext(), "Long Click", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        myFancyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String textInput = mEditText.getText().toString();
